700a371821eb4011d88e9b6ea0bb7d73bc04e45c
wuliaod1 <3093209984@qq.com>
2025-07-21 04:38:01 +0800

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // 连接失败时退出进程
+  }
+};
+
+module.exports = connectDB;
